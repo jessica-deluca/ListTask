1a59a62302d11f20d0a9b6eb37b224012f957ccc
Your Name <you@example.com>
2017-07-25 13:45:23 -0400

    added list & task models & controllers; changed display

---
+++ ListTask/Models/List.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ListTask.Models
+{
+    public class List
+    {
+        [Key]
+        public int ListID { get; set; }
+        [Display(Name = "List Title")]
+        public string ListTitle { get; set; }
+        [Display(Name = "List Created")]
+        public DateTime ListCreateDate { get; set; }
+        public virtual ICollection<Task> Tasks { get; set; }
+    }
+}
---
+++ ListTask/Models/Task.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ListTask.Models
+{
+    public class Task
+    {
+        [Key]
+        public int TaskID { get; set; }
+        [Display(Name ="Task Description")]
+        public string TaskDescription { get; set; }
+        [Display(Name = "Task Complete")]
+        public bool TaskIsDone { get; set; }
+        [Display(Name = "Task Due Date")]
+        public DateTime TaskDueDate { get; set; }
+
+        [ForeignKey("List")]
+        public int ListID { get; set; }
+        public virtual List List { get; set; }
+}
+}
